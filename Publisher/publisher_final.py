d8b005c89e658c77870057fa4885767721e5faf9
Đỗ Tuấn Phong <phong02468@gmail.com>
2024-05-20 04:39:40 +0700

    Update publisher_final.py
    Changing back from using self.counter to counter_lock when it cause duplicate counter value in publish messages.

--- Publisher/publisher_final.py
+++ Publisher/publisher_final.py
@@ -5,8 +5,9 @@ import logging
 
 BROKER = 'localhost'
 REQUEST_TOPICS = ['request/qos', 'request/delay', 'request/instancecount']
-counter = 0
+counter = 0 # Global counter variable
 logging.basicConfig(level=logging.INFO)
+counter_lock = threading.Lock() # Lock to   protect the counter variable
 
 class Publisher:
     def __init__(self, instance_id):
@@ -25,6 +26,7 @@ class Publisher:
             client.subscribe(topic)
 
     def on_message(self, client, userdata, msg):
+        # Update the QoS, delay, and instance count based on the received message
         if msg.topic == 'request/qos':
             self.qos = int(msg.payload.decode())
         elif msg.topic == 'request/delay':
@@ -33,21 +35,24 @@ class Publisher:
             requested_instance_count = int(msg.payload.decode())
             self.active = (int(self.instance_id.split('-')[1]) <= requested_instance_count)
 
-    def publish_messages(self):
+    def publish_messages(self): # Publish messages for 60 seconds
         global counter
         start_time = time.time()
 
+        # Publish messages for 60 seconds
         while time.time() - start_time < 60:
+            # Check if the publisher is active
             if not self.active:
                 break
-            with self.lock:
+            # Publish messages with a delay
+            with counter_lock:
                 topic = f'counter/{self.instance_id}/{self.qos}/{self.delay}'
                 self.client.publish(topic, f"{counter}", qos=self.qos)
                 counter += 1
             time.sleep(self.delay / 1000.0)  # Convert milliseconds to seconds
 
     def start(self):
-        self.client.connect(BROKER, 1883, 60)
+        self.client.connect(BROKER, 1883, 60)   # Connect to the broker
         self.client.loop_start()
 
         while True:
@@ -61,6 +66,7 @@ if __name__ == "__main__":
     publishers = ['pub-1', 'pub-2', 'pub-3', 'pub-4', 'pub-5']
     threads = []
 
+    # Start the publishers
     for pub_id in publishers:
         publisher = Publisher(pub_id)
         publisher_thread = threading.Thread(target=publisher.start)
